911bccd274ed7af693b00f44644ae29928e41bf9
floriankreutzer <florian.kreutzer@me.com>
2026-08-22 21:27:10 +0200

    Remove obsolete guest performance workaround

--- guest-performance-fix.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  function disableLegacyManagerObserver(){
-    const list=document.getElementById('managerList');
-    if(list) list.__guestObserved=true;
-  }
-  disableLegacyManagerObserver();
-  if(document.readyState==='loading') document.addEventListener('DOMContentLoaded',disableLegacyManagerObserver,{once:true});
-})();
