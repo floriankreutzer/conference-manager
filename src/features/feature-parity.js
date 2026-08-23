346fa68763e59d5e542249be26e5d66073da2eb7
floriankreutzer <florian.kreutzer@me.com>
2026-08-23 10:15:56 +0200

    Wire accessibility polish into employee sync

--- src/features/feature-parity.js
+++ src/features/feature-parity.js
@@ -1,6 +1,7 @@
 import { t } from '../core/i18n.js';
 import { decorateEmployeeParity, openRichFloorplan } from './employee-visuals.js';
 import { enhanceEmployeeUx } from './employee-ux.js?v=20260823-02';
+import { enhanceEmployeeAccessibilityPolish } from './employee-accessibility-polish.js?v=20260823-03';
 import { PARITY_RETURN_KEY, getAdminSection, setAdminSection } from './admin-parity.js';
 import { pt } from './parity-i18n.js';
 import { ensureParityCatalog } from './parity-data.js';
@@ -75,6 +76,7 @@ function sync() {
   restoreManagerPosition();
   decorateEmployeeParity();
   enhanceEmployeeUx();
+  enhanceEmployeeAccessibilityPolish();
   enhanceManager();
   ensureReportInsightsHeading();
   enhanceManagerResponsive();
