fa8345ccb071e642093261265718f613a71678af
floriankreutzer <florian.kreutzer@me.com>
2026-08-25 23:13:30 +0200

    Export production Employee application

--- src/employee/index.js
+++ src/employee/index.js
@@ -1,4 +1,5 @@
 export { createEmployeeApplication } from './application.js';
+export { createProductionEmployeeApplication, productionUtcInstant } from './production-application.js';
 export { decorateEmployeeParity, openRichFloorplan, requestIdFromCard } from './employee-visuals.js';
 export { enhanceEmployeeUx } from './employee-ux.js?v=20260823-02';
 export { enhanceEmployeeAccessibilityPolish } from './employee-accessibility-polish.js?v=20260823-05';
