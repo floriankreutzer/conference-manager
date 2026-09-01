d3d57d29d025552c7c873bea2222ae2a1042eff6
floriankreutzer <florian.kreutzer@me.com>
2026-09-01 09:01:08 +0200

    feat: install production inactivity lock

--- src/platform/production-bootstrap.js
+++ src/platform/production-bootstrap.js
@@ -1,8 +1,14 @@
 import { bootstrapCustomerApplication } from '../app.js';
 import { RUNTIME_MODE } from '../core/security-policy.js';
+import { installCustomerInactivityLock } from './inactivity-lock.js';
 import { bootstrapProductionAuthentication } from './production-session.js';
 
-void bootstrapCustomerApplication({
-  runtimeMode: RUNTIME_MODE.PRODUCTION,
-  authenticationBootstrap: bootstrapProductionAuthentication,
-});
+async function bootstrapProductionCustomerApplication() {
+  const application = await bootstrapCustomerApplication({
+    runtimeMode: RUNTIME_MODE.PRODUCTION,
+    authenticationBootstrap: bootstrapProductionAuthentication,
+  });
+  installCustomerInactivityLock({ context: application.context });
+}
+
+void bootstrapProductionCustomerApplication();
